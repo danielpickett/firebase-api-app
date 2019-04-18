6e60abeab02d73c2f9f31125f52dc8e1cfc2b2cf
Daniel Pickett <daniel@danielpickett.me>
2019-04-17 20:38:20 -0400

    Updates

---
+++ precache-manifest.40476992ab37f2c18c1f31a038b8aa88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7381dedd3fc1bfb38aaa",
+    "url": "/firebase-api-app/static/js/runtime~main.5d5e2c45.js"
+  },
+  {
+    "revision": "89eb8f92c5086f51f6c5",
+    "url": "/firebase-api-app/static/js/main.13a4c9a7.chunk.js"
+  },
+  {
+    "revision": "fa5d6affcb35cd06c1a7",
+    "url": "/firebase-api-app/static/js/2.97c61095.chunk.js"
+  },
+  {
+    "revision": "89eb8f92c5086f51f6c5",
+    "url": "/firebase-api-app/static/css/main.a13309ad.chunk.css"
+  },
+  {
+    "revision": "f638823d9af2a0f206cfccace08de2d1",
+    "url": "/firebase-api-app/index.html"
+  }
+];
